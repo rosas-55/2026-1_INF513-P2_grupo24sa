25883bd29a6a09654fca0ea6ffc453f9d66aeb68
rosas-55 <rosamaranon1@gmail.com>
2026-07-02 20:28:18 -0400

    Remove debugging

--- public/index.php
+++ public/index.php
@@ -17,16 +17,5 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-try {
-    $request = Illuminate\Http\Request::capture();
-    $app->handleRequest($request);
-} catch (\Throwable $e) {
-    echo "<pre>";
-    echo "Exception: " . get_class($e) . "\n";
-    echo "Message: " . $e->getMessage() . "\n";
-    echo "File: " . $e->getFile() . " on line " . $e->getLine() . "\n";
-    echo "Trace:\n" . $e->getTraceAsString() . "\n";
-    echo "</pre>";
-    exit;
-}
+$app->handleRequest(Request::capture());
 
