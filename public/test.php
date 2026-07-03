8e4975ed6142239407890bbf760e08637584a3e0
rosas-55 <rosamaranon1@gmail.com>
2026-07-02 20:16:07 -0400

    Update test.php with Laravel request parsing

--- public/test.php
+++ public/test.php
@@ -1,7 +1,11 @@
 <?php
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+$request = Illuminate\Http\Request::capture();
+
 echo "<pre>";
-echo "REQUEST_URI: " . (isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : 'N/A') . "\n";
-echo "SCRIPT_NAME: " . (isset($_SERVER['SCRIPT_NAME']) ? $_SERVER['SCRIPT_NAME'] : 'N/A') . "\n";
-echo "REQUEST_METHOD: " . (isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'N/A') . "\n";
-echo "PHP_SELF: " . (isset($_SERVER['PHP_SELF']) ? $_SERVER['PHP_SELF'] : 'N/A') . "\n";
+echo "URL: " . $request->url() . "\n";
+echo "BaseUrl: " . $request->getBaseUrl() . "\n";
+echo "PathInfo: " . $request->getPathInfo() . "\n";
+echo "Method: " . $request->method() . "\n";
 echo "</pre>";
