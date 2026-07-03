a9b2c36c87e3bde3268543f8458ccfd4aca42af6
rosas-55 <rosamaranon1@gmail.com>
2026-07-02 20:24:20 -0400

    Intercept index.php to print stack trace

--- public/index.php
+++ public/index.php
@@ -17,13 +17,16 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$request = Illuminate\Http\Request::capture();
-
-echo "<pre>";
-echo "URL: " . $request->url() . "\n";
-echo "BaseUrl: " . $request->getBaseUrl() . "\n";
-echo "PathInfo: " . $request->getPathInfo() . "\n";
-echo "Method: " . $request->method() . "\n";
-echo "</pre>";
-exit;
+try {
+    $request = Illuminate\Http\Request::capture();
+    $app->handleRequest($request);
+} catch (\Throwable $e) {
+    echo "<pre>";
+    echo "Exception: " . get_class($e) . "\n";
+    echo "Message: " . $e->getMessage() . "\n";
+    echo "File: " . $e->getFile() . " on line " . $e->getLine() . "\n";
+    echo "Trace:\n" . $e->getTraceAsString() . "\n";
+    echo "</pre>";
+    exit;
+}
 
